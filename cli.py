4250373b6e4f52f5fae7fefc7df8a7c9c16206e6
Danilo <danilo.merlo@sou.inteli.edu.br>
2025-02-14 13:31:23 -0300

    Versão inicial CLI

---
+++ cli.py
@@ -0,0 +1,78 @@
+import typer
+from yaspin import yaspin
+import json
+
+from typing_extensions import Annotated
+from dobot.dobotController import DobotController
+from dobot.position import Position
+
+cli = typer.Typer()
+dobot = DobotController()
+
+
+@cli.command()
+def move_to(
+    x: Annotated[float, typer.Argument(help="X coordinate to move to")],
+    y: Annotated[float, typer.Argument(help="Y coordinate to move to")],
+    z: Annotated[float, typer.Argument(help="Z coordinate to move to")],
+    r: Annotated[float, typer.Argument(help="R coordinate to move to")],
+    wait: Annotated[bool, typer.Option(help="Wait for the movement to finish")] = True,
+):
+    dobot.move_to(Position(x, y, z, r), wait)
+
+
+@cli.command()
+def home(
+    wait: Annotated[bool, typer.Option(help="Wait for the movement to finish")] = True
+):
+    dobot.home(wait)
+
+
+@cli.command()
+def set_home(
+    x: Annotated[float, typer.Argument(help="X coordinate to set home")],
+    y: Annotated[float, typer.Argument(help="Y coordinate to set home")],
+    z: Annotated[float, typer.Argument(help="Z coordinate to set home")],
+    r: Annotated[float, typer.Argument(help="R coordinate to set home")],
+):
+    dobot.set_home(Position(x, y, z, r))
+
+@cli.command()
+def enable_tool(
+    time: Annotated[float, typer.Option(help='Wait time')]
+):
+    dobot.enable_tool(time)
+    
+@cli.command()
+def disable_tool(
+    time: Annotated[float, typer.Option(help='Wait time')]
+):
+    dobot.disable_tool(time)
+    
+@cli.command()
+def current():
+    dobot.pose()
+
+@cli.command()
+def set_speed(
+    speed: Annotated[float, typer.Option(help='Dobot speed')],
+    acceleration: Annotated[float, typer.Option(help='Dobot acceleration')]
+):
+    dobot.set_speed(speed, acceleration)
+
+# @cli.command()
+# def save():
+
+# @cli.command()
+# def run():
+
+def main():
+    port = '/dev/ttyACM0'
+    spinner = yaspin(text=f"Connecting with port {port}...")
+    spinner.start()
+    dobot.connect(port)
+    spinner.stop()
+    cli()
+
+if __name__ == '__main__':
+    main()
---
+++ dobot/dobotController.py
@@ -0,0 +1,45 @@
+import pydobot
+from .position import Position
+
+class DobotController:
+    def __init__(self):
+        self.tool_enable = False
+        self.home_position = Position(
+            100.0, 0.0, 150.0, 0.0, 0.0, 0.0, 0.0, 0.0, False, False
+        )
+        self.conected = False
+
+    def connect(self, port):
+        self.dobot = pydobot.Dobot(port=port, verbose=False)
+        self.conect = True
+        
+    def disconect(self):
+        self.dobot.close
+        self.conected = False
+
+    def pose(self):
+        current_position = Position(*self.dobot.pose())
+        current_position.suck == self.tool_enable
+        return current_position
+    
+    def set_speed(self, speed, acceleration):
+        self.dobot.speed(speed, acceleration)
+
+    def move_to(self, position, wait=True):
+        self.dobot.move_to(*position.to_list(), wait=wait)
+
+    def set_home(self, position):
+        self.home = position
+
+    def home(self, wait=True):
+        self.move_to(self.home, wait=wait)
+        
+    def enable_tool(self, time):
+        self.dobot.suck(True)
+        self.dobot.wait(time)
+        self.tool_enabled = True
+
+    def disable_tool(self, time):
+        self.dobot.suck(False)
+        self.dobot.wait(time)
+        self.tool_enabled = False
